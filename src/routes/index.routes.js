e785786a6388db55152e1cda63ffc9a4ef4f6cc1
tomy07511 <sopadejesuss@gmail.com>
2023-08-02 23:36:29 -0500

    sopa

---
+++ src/routes/index.routes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+// Controllers
+const { renderIndex, renderAbout, renderHome, renderpedir, renderInfo,renderdatos } = require("../controllers/index.controller");
+const {guardarDtos } = require("../controllers/Dtos.controller")
+
+
+router.get("/", renderIndex);
+router.get("/about", renderAbout);
+router.get("/home", renderHome);
+router.get("/pedir", renderpedir);
+router.get("/Info", renderInfo);
+router.get("/datos", renderdatos);
+router.post("/Dtos", guardarDtos);
+
+
+module.exports = router;
